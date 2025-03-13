ae8e8cef5650bfa9a38568ed398019b306caca23
zach <senpaiswag422@gmail.com>
2025-03-14 01:42:44 +0800

    Online Connection
    only connecting players, game not connected

---
+++ Assets/Scripts/Network/CharacterSelect.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class CharacterSelect
+{
+    public const string PLAYER_READY = "IsPlayerReady";     
+    public const string PLAYER_SELECTION_NUMBER = "Player_Selection_Number";     
+}
+
