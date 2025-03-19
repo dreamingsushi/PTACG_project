c6ef79bc6bdefc7c21490bcf8712115c766e060e
Ico506 <icothestorylingerer@gmail.com>
2025-03-20 05:55:20 +0800

    InteractiveInLevels

---
+++ Assets/Damian'sAssets/DamianScripts/CannonManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonManager : MonoBehaviour
+{
+    public Animator[] Cannonset1;
+    public Animator[] Cannonset2;
+    public Animator[] Cannonset3;
+    public float delaytime;
+
+    void Start()
+    {
+        Invoke("Cannonattack1", delaytime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Cannonattack1()
+    {
+        Debug.Log("aobin");
+        foreach (Animator anim in Cannonset1)
+        {
+
+            anim.SetTrigger("Fall");
+            Invoke("Cannonattack2", delaytime);
+        }
+    }
+
+    void Cannonattack2()
+    {
+        foreach (Animator anim in Cannonset2)
+        {
+
+            anim.SetTrigger("Fall");
+            Invoke("Cannonattack3", delaytime);
+        }
+    }
+
+    void Cannonattack3()
+    {
+        foreach (Animator anim in Cannonset3)
+        {
+
+            anim.SetTrigger("Fall");
+            Invoke("Cannonattack1", delaytime);
+        }
+    }
+
+}
---
+++ Assets/Damian'sAssets/DamianScripts/UFOManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOManager : MonoBehaviour
+{
+    public Animator[] UFOset1;
+    public Animator[] UFOset2;
+    public Animator[] UFOset3;
+    public Animator[] UFOset4;
+    public float delaytime;
+
+    void Start()
+    {
+        Invoke("UFOattack1", delaytime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UFOattack1()
+    {
+        foreach (Animator anim in UFOset1)
+        {
+            
+            anim.SetTrigger("Attack");
+            Invoke("UFOattack2", delaytime);
+        }
+    }
+
+    void UFOattack2()
+    {
+        foreach (Animator anim in UFOset2)
+        {
+            
+            anim.SetTrigger("Attack");
+            Invoke("UFOattack3", delaytime);
+        }
+    }
+
+    void UFOattack3()
+    {
+        foreach (Animator anim in UFOset3)
+        {
+            
+            anim.SetTrigger("Attack");
+            Invoke("UFOattack4", delaytime);
+        }
+    }
+
+    void UFOattack4()
+    {
+        foreach (Animator anim in UFOset4)
+        {
+            
+            anim.SetTrigger("Attack");
+            Invoke("UFOattack1", delaytime);
+        }
+    }
+}
