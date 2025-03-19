fe294a88a485154db14f198eda129619fd8e2d62
zach <senpaiswag422@gmail.com>
2025-03-20 05:26:35 +0800

    Bugfix1.2

--- Assets/EndScreen.cs
+++ Assets/EndScreen.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndScreen : MonoBehaviour
+public class EndScreen : MonoBehaviourPunCallbacks
 {
+
+
     public void ReturnToMainMenuWorld()
     {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
         SceneManager.LoadScene(0);
+        base.OnLeftRoom();
     }
 }
